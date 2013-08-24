f4840291db4d581bb2b0053103b42a09bb145871
Jonas Gorauskas <jgorauskas@gmail.com>
2013-08-23 23:27:28 -0700

    Unzip compressed archives in a certain folder

---
+++ python/unzip.py
@@ -0,0 +1,28 @@
+
+# Unzip all compressed archives in a directory
+
+import zipfile
+import os
+
+d = r'C:\ted'
+dl = os.listdir(d)
+
+def unzipme(x):
+    f = os.path.join(d, x)
+    zf = zipfile.ZipFile(f)
+
+    print 'Unzipping %s to .\\' % f,
+
+    for i, fn in enumerate(zf.namelist()):
+        of = open(os.path.join(d, fn), 'wb')
+        print '%s ...' % fn,
+        of.write(zf.read(fn))
+        of.flush()
+        of.close()
+
+    print 'Done!'
+    
+    
+map(unzipme, dl)
+
+
