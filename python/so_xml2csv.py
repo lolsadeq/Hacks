1ea0799bd3ea6179d70614274f7e86e74a9c74c0
Jonas Gorauskas <jgorauskas@gmail.com>
2014-09-01 16:12:37 -0700

    adding file header

--- python/so_xml2csv.py
+++ python/so_xml2csv.py
@@ -1,6 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+"""
+so_xml2csv.py - Converts stackoverflow xml dump data to csv format
+Author: Jonas Gorauskas
+Created: 2014-09-01 13:53:06
+Modified: 2014-09-01 16:12:04
+Description:
 
+This script converts stackoverflow xml dump data to csv format for the purposes
+of easy insertion into a SQL database such as Postgreql.
+
+History:
+
+    2014-09-01 13:53:06 - JGG
+        Initial commit
+
+"""
 
 def main():
     pass
